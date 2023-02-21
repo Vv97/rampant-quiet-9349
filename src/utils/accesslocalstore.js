e65b1c1b235cad99d86302c4e71a5c59204b9222
Vv97 <VishalVarma537@gmail.com>
2023-02-21 12:23:38 +0530

    package installed

---
+++ src/utils/accesslocalstore.js
@@ -0,0 +1,13 @@
+export const getLocalData = (key) => {
+    try {
+        let temp = JSON.parse(localStorage.getItem(key))
+        return temp
+    } catch (e) {
+        return undefined
+    }
+}
+
+
+export const setLocalDate = (key, data) => {
+    return localStorage.setItem(key, JSON.stringify(data))
+}
