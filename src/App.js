453a4ccb71e6b73fca3555e0542909e59bfb88f8
Vv97 <VishalVarma537@gmail.com>
2023-02-22 22:54:04 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { AllRoutes } from './Routes/AllRoutes';
-import { Login } from './pages/Loginpage/Login';
-import { Register } from './pages/RegisterPage/Register';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <AllRoutes />
-      <Login /> */}
-      <Register />
+      <AllRoutes />
     </div>
   );
 }
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import CartPage from "../pages/Cart/CartPage";
 import { Home } from "../pages/Home/Home";
+import { Login } from "../pages/Loginpage/Login";
+import { Register } from "../pages/RegisterPage/Register";
 
 export const AllRoutes = () => {
   return (
@@ -9,6 +11,8 @@ export const AllRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="*" element={<h1>page not found</h1>} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
