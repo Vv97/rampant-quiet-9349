ef7485d2c41319bb04b98a4eed2410fcd1d9f76a
Vv97 <VishalVarma537@gmail.com>
2023-02-22 22:39:31 +0530

    chakra-icons

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -7,8 +7,8 @@ export const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cart" element={<CartPage/>}/>
-
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="*" element={<h1>page not found</h1>} />
     </Routes>
   );
 };
