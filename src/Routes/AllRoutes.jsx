2970c492ae4f2b55d37addc61257ce4e2d416713
Rushijoshi3798 <rushijoshi3798@gmail.com>
2023-02-23 22:24:15 +0530

    Rushi- day-3

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Admin from "../components/Rushi/Admin/Admin";
+import AdminProducts from "../components/Rushi/Admin/AdminProducts";
 import CartPage from "../pages/Cart/CartPage";
 import { Home } from "../pages/Home/Home";
 import { Login } from "../pages/Loginpage/Login";
@@ -10,9 +12,11 @@ export const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<CartPage />} />
+      <Route path="/admin" element={<Admin />} />
       <Route path="*" element={<h1>page not found</h1>} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/admin_products" element={<AdminProducts/>}/>
     </Routes>
   );
 };
--- src/components/Rushi/Admin/Admin.jsx
+++ src/components/Rushi/Admin/Admin.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import NavbarAdmin from './Navbar-admin'
-import { ChakraProvider } from "@chakra-ui/react"
-
+import React from "react";
+import NavbarAdmin from "./Navbar-admin";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./Admin.css";
+import {Button} from "@chakra-ui/react";
 const Admin = () => {
+
+
   return (
     <div>
       <ChakraProvider>
-      <NavbarAdmin />
-      Admin page
+        <NavbarAdmin />
+        <div className="admin_dashboard_div">
+          <div className="welcome_div">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h3 className="animate-charcter">Welcome</h3>
+                <h4 className="eBuzz_text">eBuzz is here to make your bussiness grow!</h4>
+              </div>
+            </div>
+          </div>
+          <div className="explore_button">
+            <Button>Explore Now!</Button>
+          </div>
+          <div className="footer">Copyright © 1995-2023 eBay Inc. All Rights Reserved. Accessibility, User Agreement, Privacy, Payments Terms of Use, Cookies, Your Privacy Choices and AdChoice</div>
+        </div>
       </ChakraProvider>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
