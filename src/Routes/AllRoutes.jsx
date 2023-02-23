5006f88ae0f3c1a5720345a8b13cd29d6c29f1e5
Kashif Jamal <kashifjamal30@gmail.com>
2023-02-23 07:19:53 +0530

    done

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
+import Product from "../pages/Product";
 
 export const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Product />}></Route>
     </Routes>
   );
 };
