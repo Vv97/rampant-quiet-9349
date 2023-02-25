7c23725e2c96af0e77e0fa2903b211761ab11eee
Vv97 <VishalVarma537@gmail.com>
2023-02-25 15:32:08 +0530

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Carousal } from "../../components/fw21_0631/Carusol/Carousal";
 import { CatNav } from "../../components/fw21_0631/Cat-nav/CatNav";
+import { Customcard } from "../../components/fw21_0631/customcard/Customcard";
 import { DailyCarousal } from "../../components/fw21_0631/DailyCarousal/DailyCarousal";
 import { Footer } from "../../components/fw21_0631/Footer/Footer";
 import { Navbar } from "../../components/fw21_0631/Navbar/Navbar";
@@ -18,6 +19,10 @@ export const Home = () => {
 
       <div>
         <Carousal />
+      </div>
+
+      <div>
+        <Customcard />
       </div>
 
       <div>
